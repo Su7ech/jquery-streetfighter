6b208058432488e22db5b14fb581cc6a78afb1df
Su7ech <tpbaroody@gmail.com>
2015-08-03 14:36:12 -0700

    Added jQuery Animations

--- js/app.js
+++ js/app.js
@@ -3,18 +3,17 @@ $(document).ready(function() {
 });
 
 function playGame() {
-	$('.ryu').mouseover(function() {
+	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
+	}).mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	})
-	.mousedown(function() {
+	}).mousedown(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
+		
 		$('.hadouken').finish().show().animate(
 			{'left': '300px'},
 			500, 
@@ -24,11 +23,23 @@ function playGame() {
 				$(this).css('left', '-264px');
 			}
 		);
-	})
-	.mouseup(function() {
+	}).mouseup(function() {
 		$('.ryu-throwing').hide();
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
+		$('.ryu-still').show();
 		$('.hadouken').hide();
+	});
+
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-still').hide();
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').hide();
+			$('.ryu-cool').show();
+		}
+	}).keyup(function(e) {
+		if (e.keyCode == 88) {
+			$('.ryu-cool').hide();
+			$('.ryu-still').show();
+		}
 	});
 }
